09dd23e7481e3e69cd4ac0d34c55bce8b34a2df6
Sergey Ivanov <voivoid@mail.ru>
2018-11-17 10:54:12 +0300

    add a bit of vs support

--- cmake/build_utils.cmake
+++ cmake/build_utils.cmake
@@ -2,6 +2,8 @@ include_guard()
 
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
   set(AocCxxWarnings -Wall -Wextra -Wpedantic -Werror -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wrestrict -Wnull-dereference -Wold-style-cast -Wuseless-cast -Wdouble-promotion -Wshadow -Wformat=2 -Wconversion -Wsign-conversion -Wfloat-equal -Wcast-qual -Winit-self -Wpointer-arith)
+elseif (MSVC)
+  set(AocCxxWarnings /permissive- /W2 /WX)
 endif()
 
 macro(process_aoc_target Target)
