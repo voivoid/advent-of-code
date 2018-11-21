0a033ba11de35b55c1ce79b4eb3bde93e0f4e803
Sergey Ivanov <voivoid@mail.ru>
2018-11-21 21:08:43 +0300

    2017_17 optimized

--- src/2015/problem_17.cpp
+++ src/2015/problem_17.cpp
@@ -5,6 +5,7 @@
 
 #include "range/v3/action/sort.hpp"
 #include "range/v3/algorithm/min.hpp"
+#include "range/v3/algorithm/lexicographical_compare.hpp"
 #include "range/v3/distance.hpp"
 #include "range/v3/istream_range.hpp"
 #include "range/v3/numeric/accumulate.hpp"
@@ -18,15 +19,42 @@
 #include "range/v3/view/transform.hpp"
 
 #include <istream>
+#include <map>
 
 namespace
 {
 using Volume                 = int;
 using ContainersCombinations = ranges::any_view<ranges::any_view<Volume>>;
+using Containers             = std::vector<Volume>;
+using CacheKey               = std::pair<Volume, ranges::any_view<Volume, ranges::category::random_access>>;
 
-ContainersCombinations generate_combinations( ranges::any_view<Volume, ranges::category::random_access> containers,
-                                              const Volume volume_left )
+struct CacheComparator
 {
+  bool operator()( const CacheKey& k1, const CacheKey& k2 ) const
+  {
+    if( k1.first != k2.first )
+    {
+        return k1.first < k2.first;
+    }
+
+    auto r1 = k1.second;
+    auto r2 = k2.second;
+
+    return ranges::lexicographical_compare( r1, r2 );
+  }
+};
+
+using Cache = std::map<CacheKey, ContainersCombinations, CacheComparator>;
+
+ContainersCombinations
+    generate_combinations( ranges::any_view<Volume, ranges::category::random_access> containers, const Volume volume_left, Cache& cache )
+{
+  const auto cache_iter = cache.find( std::make_pair( volume_left, containers ) );
+  if ( cache_iter != cache.cend() )
+  {
+    return cache_iter->second;
+  }
+
   if ( volume_left == 0 )
   {
     return ranges::view::single( ranges::view::empty<Volume>() );
@@ -43,20 +71,25 @@ ContainersCombinations generate_combinations( ranges::any_view<Volume, ranges::c
   }
 
   auto tail = containers | ranges::view::tail;
-  ContainersCombinations result1 =
-      generate_combinations( tail, volume_left - head ) |
-      ranges::view::transform( [head]( auto rng ) { return ranges::view::concat( ranges::view::single( head ), rng ); } ) |
-      ranges::view::filter( [volume_left]( auto rng ) { return ranges::accumulate( rng, 0 ) == volume_left; } );
 
-  ContainersCombinations result2 = generate_combinations( tail, volume_left );
+  ContainersCombinations result1 = generate_combinations( tail, volume_left - head, cache ) | ranges::view::transform( [head]( auto rng ) {
+                                     return ranges::view::concat( ranges::view::single( head ), rng );
+                                   } );
 
-  return ranges::view::concat( result1, result2 );
+  ContainersCombinations result2 = generate_combinations( tail, volume_left, cache );
+
+  auto result = ranges::view::concat( result1, result2 );
+
+  cache.emplace( std::make_pair( volume_left, containers ), result );
+
+  return result;
 }
 
 ContainersCombinations generate_combinations( std::istream& input, const Volume volume_to_store )
 {
   auto containers = ranges::istream_range<Volume>( input ) | ranges::to_vector | ranges::action::sort;
-  return generate_combinations( containers, volume_to_store );
+  Cache cache;
+  return generate_combinations( containers, volume_to_store, cache );
 }
 
 auto get_possible_combinations_num( std::istream& input, const Volume volume_to_store )
@@ -112,41 +145,47 @@ AOC_REGISTER_PROBLEM( 2015_17, solve_1, solve_2 );
 
 static void impl_tests()
 {
-  using VV = std::vector<std::vector<Volume>>;
+  using VV = std::vector<Containers>;
 
   {
     std::vector<Volume> v{};
-    auto r = generate_combinations( v, 1 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 1, cache ) | AoC::to_2d_vector();
     assert( r == VV{} );
   }
 
   {
     std::vector<Volume> v{ 1 };
-    auto r = generate_combinations( v, 0 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 0, cache ) | AoC::to_2d_vector();
     assert( r == VV{ {} } );
   }
 
   {
     std::vector<Volume> v{ 3 };
-    auto r = generate_combinations( v, 1 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 1, cache ) | AoC::to_2d_vector();
     assert( r == VV{} );
   }
 
   {
     std::vector<Volume> v{ 3 };
-    auto r = generate_combinations( v, 3 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 3, cache ) | AoC::to_2d_vector();
     assert( r == VV{ { 3 } } );
   }
 
   {
     std::vector<Volume> v{ 2, 3, 5 };
-    auto r = generate_combinations( v, 10 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 10, cache ) | AoC::to_2d_vector();
     assert( r == ( VV{ { 2, 3, 5 } } ) );
   }
 
   {
     std::vector<Volume> v{ 5, 5, 100, 100 };
-    auto r = generate_combinations( v, 10 ) | AoC::to_2d_vector();
+    Cache cache;
+    auto r = generate_combinations( v, 10, cache ) | AoC::to_2d_vector();
     assert( r == ( VV{ { 5, 5 } } ) );
   }
 
