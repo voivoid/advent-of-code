a754150b1f8f2dc468cfbe8b31432abfa4790d56
Sergey Ivanov <voivoid@mail.ru>
2019-07-25 22:02:41 +0300

    update msvc build

--- utils/inc/AoC/utils/assert.h
+++ utils/inc/AoC/utils/assert.h
@@ -4,8 +4,10 @@
 
 #include "boost/preprocessor/stringize.hpp"
 
+#define AOC_UNEXPECTED() throw std::runtime_error( __FILE__ ":" BOOST_PP_STRINGIZE( __LINE__ ) ": This should never be executed" )
+
 #ifdef NDEBUG
-#  define AOC_ASSERT_FALSE() throw std::runtime_error( __FILE__ ":" BOOST_PP_STRINGIZE( __LINE__ ) ": This should never be executed" )
+#  define AOC_ASSERT_FALSE() AOC_UNEXPECTED();
 #else
-#  define AOC_ASSERT_FALSE() assert( false )
+#  define AOC_ASSERT_FALSE() { assert( false ); AOC_UNEXPECTED(); }
 #endif
