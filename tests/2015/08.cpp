0a3ef10794eaa56b190ae937d4c377847497b221
Sergey Ivanov <voivoid@mail.ru>
2020-03-19 23:36:03 +0300

    fix MSVC

--- tests/2015/08.cpp
+++ tests/2015/08.cpp
@@ -11,7 +11,7 @@ BOOST_FIXTURE_TEST_CASE( problem2015_08_1, AocFixture )
   BOOST_CHECK_EQUAL( 2, run( &solve_1, "\"abc\"" ) );
   BOOST_CHECK_EQUAL( 3, run( &solve_1, "\"aaa\"aaa\"" ) );
   BOOST_CHECK_EQUAL( 5, run( &solve_1, "\"\\x27\"" ) );
-  BOOST_CHECK_EQUAL( 9, run( &solve_1, R"("sjdivfriyaaqa\xd2v\"k\"mpcu\"yyu\"en")" ) );
+  BOOST_CHECK_EQUAL( 9, run( &solve_1, "\"sjdivfriyaaqa\\xd2v\\\"k\\\"mpcu\\\"yyu\\\"en\"" ) );
 }
 
 BOOST_FIXTURE_TEST_CASE( problem2015_08_2, AocFixture )
